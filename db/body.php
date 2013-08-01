f1dfaab14fde15b7a29059b7b6294ebb48e45dbb
Vincent Thibault <vthibault.mobile@gmail.com>
2013-08-02 01:42:20 +0200

    Update 3rd job filename
    Update 3rd job file's name.
    Convert the file to don't have problem with charset (github and host).

--- db/body.php
+++ db/body.php
@@ -7,80 +7,80 @@
 */
 
 return array(
-	0 => "ÃÊº¸ÀÚ",
-	1 => "°Ë»ç",                    4002 => "°Ë»ç",       4024 => "°Ë»ç",
-	2 => "¸¶¹ý»ç",                  4003 => "¸¶¹ý»ç",     4025 => "¸¶¹ý»ç",
-	3 => "±Ã¼ö",                  4004 => "±Ã¼ö",     4026 => "±Ã¼ö",
-	4 => "¼ºÁ÷ÀÚ",                4005 => "¼ºÁ÷ÀÚ",   4027 => "¼ºÁ÷ÀÚ",
-	5 => "»óÀÎ",                    4006 => "»óÀÎ",       4028 => "»óÀÎ",
-	6 => "µµµÏ",                    4007 => "µµµÏ",       4029 => "µµµÏ",
-	7 => "±â»ç",                    4030 => "±â»ç",
-	8 => "ÇÁ¸®½ºÆ®",              4031 => "ÇÁ¸®½ºÆ®",
-	9 => "À§Àúµå",                  4032 => "À§Àúµå",
-	10 => "Á¦Ã¶°ø",                 4033 => "Á¦Ã¶°ø",
-	11 => "ÇåÅÍ",                   4034 => "ÇåÅÍ",
-	12 => "¾î¼¼½Å",         4035 => "¾î¼¼½Å",
-	13 => "ÆäÄÚÆäÄÚ_±â»ç",          4036 => "ÆäÄÚÆäÄÚ_±â»ç",
-	14 => "Å©·ç¼¼ÀÌ´õ",         4037 => "Å©·ç¼¼ÀÌ´õ",
-	15 => "¸ùÅ©",                   4038 => "¸ùÅ©",
-	16 => "¼¼ÀÌÁö",             4039 => "¼¼ÀÌÁö",
-	17 => "·Î±×",                   4040 => "·Î±×",
-	18 => "¿¬±Ý¼ú»ç",             4041 => "¿¬±Ý¼ú»ç",
-	19 => "¹Ùµå",                   4042 => "¹Ùµå",
-	20 => "¹«Èñ",                   4043 => "¹«Èñ",
-	21 => "½ÅÆäÄÚÅ©·ç¼¼ÀÌ´õ", 4044 => "½ÅÆäÄÚÅ©·ç¼¼ÀÌ´õ",
-	22 => "°áÈ¥",
-	23 => "½´ÆÛ³ëºñ½º",           4045 => "½´ÆÛ³ëºñ½º",
-	24 => "°Ç³Ê",
-	25 => "´ÑÀÚ",
-	26 => "»êÅ¸",
-	27 => "¿©¸§",
+	0  => "\xc3\xca\xba\xb8\xc0\xda",
+	1  => "\xb0\xcb\xbb\xe7",                    4002 => "\xb0\xcb\xbb\xe7",          4024 => "\xb0\xcb\xbb\xe7",
+	2  => "\xb8\xb6\xb9\xfd\xbb\xe7",            4003 => "\xb8\xb6\xb9\xfd\xbb\xe7",  4025 => "\xb8\xb6\xb9\xfd\xbb\xe7",
+	3  => "\xb1\xc3\xbc\xf6",                    4004 => "\xb1\xc3\xbc\xf6",          4026 => "\xb1\xc3\xbc\xf6",
+	4  => "\xbc\xba\xc1\xf7\xc0\xda",            4005 => "\xbc\xba\xc1\xf7\xc0\xda",  4027 => "\xbc\xba\xc1\xf7\xc0\xda",
+	5  => "\xbb\xf3\xc0\xce",                    4006 => "\xbb\xf3\xc0\xce",          4028 => "\xbb\xf3\xc0\xce",
+	6  => "\xb5\xb5\xb5\xcf",                    4007 => "\xb5\xb5\xb5\xcf",          4029 => "\xb5\xb5\xb5\xcf",
+	7  => "\xb1\xe2\xbb\xe7",                    4030 => "\xb1\xe2\xbb\xe7",
+	8  => "\xc7\xc1\xb8\xae\xbd\xba\xc6\xae",    4031 => "\xc7\xc1\xb8\xae\xbd\xba\xc6\xae",
+	9  => "\xc0\xa7\xc0\xfa\xb5\xe5",            4032 => "\xc0\xa7\xc0\xfa\xb5\xe5",
+	10 => "\xc1\xa6\xc3\xb6\xb0\xf8",            4033 => "\xc1\xa6\xc3\xb6\xb0\xf8",
+	11 => "\xc7\xe5\xc5\xcd",                    4034 => "\xc7\xe5\xc5\xcd",
+	12 => "\xbe\xee\xbc\xbc\xbd\xc5",            4035 => "\xbe\xee\xbc\xbc\xbd\xc5",
+	13 => "\xc6\xe4\xc4\xda\xc6\xe4\xc4\xda\x5f\xb1\xe2\xbb\xe7",             4036 => "\xc6\xe4\xc4\xda\xc6\xe4\xc4\xda\x5f\xb1\xe2\xbb\xe7",
+	14 => "\xc5\xa9\xb7\xe7\xbc\xbc\xc0\xcc\xb4\xf5",                         4037 => "\xc5\xa9\xb7\xe7\xbc\xbc\xc0\xcc\xb4\xf5",
+	15 => "\xb8\xf9\xc5\xa9",                                                 4038 => "\xb8\xf9\xc5\xa9",
+	16 => "\xbc\xbc\xc0\xcc\xc1\xf6",                                         4039 => "\xbc\xbc\xc0\xcc\xc1\xf6",
+	17 => "\xb7\xce\xb1\xd7",                                                 4040 =>"\xb7\xce\xb1\xd7",
+	18 => "\xbf\xac\xb1\xdd\xbc\xfa\xbb\xe7",                                 4041 => "\xbf\xac\xb1\xdd\xbc\xfa\xbb\xe7",
+	19 => "\xb9\xd9\xb5\xe5",                                                 4042 => "\xb9\xd9\xb5\xe5",
+	20 => "\xb9\xab\xc8\xf1",                                                 4043 => "\xb9\xab\xc8\xf1",
+	21 => "\xbd\xc5\xc6\xe4\xc4\xda\xc5\xa9\xb7\xe7\xbc\xbc\xc0\xcc\xb4\xf5", 4044 => "\xbd\xc5\xc6\xe4\xc4\xda\xc5\xa9\xb7\xe7\xbc\xbc\xc0\xcc\xb4\xf5",
+	22 => "\xb0\xe1\xc8\xa5",
+	23 => "\xbd\xb4\xc6\xdb\xb3\xeb\xba\xf1\xbd\xba",                         4045 => "\xbd\xb4\xc6\xdb\xb3\xeb\xba\xf1\xbd\xba",
+	24 => "\xb0\xc7\xb3\xca",
+	25 => "\xb4\xd1\xc0\xda",
+	26 => "\xbb\xea\xc5\xb8",
+	27 => "\xbf\xa9\xb8\xa7",
 
-	4008 => "·Îµå³ªÀÌÆ®",
-	4009 => "ÇÏÀÌÇÁ¸®",
-	4010 => "ÇÏÀÌÀ§Àúµå",
-	4011 => "È­ÀÌÆ®½º¹Ì½º",
-	4012 => "½º³ªÀÌÆÛ",
-	4013 => "¾î½Ø½ÅÅ©·Î½º",
-	4014 => "·ÎµåÆäÄÚ",
-	4015 => "ÆÈ¶óµò",
-	4016 => "Ã¨ÇÇ¿Â",
-	4017 => "ÇÁ·ÎÆä¼­",
-	4018 => "½ºÅäÄ¿",
-	4019 => "Å©¸®¿¡ÀÌÅÍ",
-	4020 => "Å¬¶ó¿î",
-	4021 => "Áý½Ã",
-	4022 => "ÆäÄÚÆÈ¶óµò",
+	4008 => "\xb7\xce\xb5\xe5\xb3\xaa\xc0\xcc\xc6\xae",
+	4009 => "\xc7\xcf\xc0\xcc\xc7\xc1\xb8\xae",
+	4010 => "\xc7\xcf\xc0\xcc\xc0\xa7\xc0\xfa\xb5\xe5",
+	4011 => "\xc8\xad\xc0\xcc\xc6\xae\xbd\xba\xb9\xcc\xbd\xba",
+	4012 => "\xbd\xba\xb3\xaa\xc0\xcc\xc6\xdb",
+	4013 => "\xbe\xee\xbd\xd8\xbd\xc5\xc5\xa9\xb7\xce\xbd\xba",
+	4014 => "\xb7\xce\xb5\xe5\xc6\xe4\xc4\xda",
+	4015 => "\xc6\xc8\xb6\xf3\xb5\xf2",
+	4016 => "\xc3\xa8\xc7\xc7\xbf\xc2",
+	4017 => "\xc7\xc1\xb7\xce\xc6\xe4\xbc\xad",
+	4018 => "\xbd\xba\xc5\xe4\xc4\xbf",
+	4019 => "\xc5\xa9\xb8\xae\xbf\xa1\xc0\xcc\xc5\xcd",
+	4020 => "\xc5\xac\xb6\xf3\xbf\xee",
+	4021 => "\xc1\xfd\xbd\xc3",
+	4022 => "\xc6\xe4\xc4\xda\xc6\xc8\xb6\xf3\xb5\xf2",
 
-	4046 => "ÅÂ±Ç¼Ò³â",
-	4047 => "±Ç¼º",
-	4048 => "±Ç¼ºÀ¶ÇÕ",
-	4049 => "¼Ò¿ï¸µÄ¿",
+	4046 => "\xc5\xc2\xb1\xc7\xbc\xd2\xb3\xe2",
+	4047 => "\xb1\xc7\xbc\xba",
+	4048 => "\xb1\xc7\xbc\xba\xc0\xb6\xc7\xd5",
+	4049 => "\xbc\xd2\xbf\xef\xb8\xb5\xc4\xbf",
 
 
-	//       normal                extend                        baby
-	4054 => "·é³ªÀÌÆ®",         4060 => "·é³ªÀÌÆ®",         4096 => "·é³ªÀÌÆ®",         // Rune_Knight
-	4055 => "¿ö·Ï",             4061 => "¿ö·Ï",             4097 => "¿ö·Ï",             // Warlock
-	4056 => "·¹ÀÎÁ®",           4062 => "·¹ÀÎÁ®",           4098 => "·¹ÀÎÁ®",           // Ranger
-	4057 => "¾ÆÅ©ºñ¼ó",     4063 => "¾ÆÅ©ºñ¼ó",     4099 => "¾ÆÅ©ºñ¼ó",     // Arch_Bishop
-	4058 => "¹ÌÄÉ´Ð",           4064 => "¹ÌÄÉ´Ð",           4100 => "¹ÌÄÉ´Ð",           // Mechanic
-	4059 => "±æ·ÎÆ¾Å©·Î½º", 4065 => "±æ·ÎÆ¾Å©·Î½º", 4101 => "±æ·ÎÆ¾Å©·Î½º", // Guillotine_Cross
+	//                normal                                                    extend                                                   baby
+	4054 => "\xb7\xe9\xb3\xaa\xc0\xcc\xc6\xae",                 4060 => "\xb7\xe9\xb3\xaa\xc0\xcc\xc6\xae",                  4096 => "\xb7\xe9\xb3\xaa\xc0\xcc\xc6\xae",     // Rune_Knight
+	4055 => "\xbf\xf6\xb7\xcf",                                 4061 => "\xbf\xf6\xb7\xcf",                                  4097 => "\xbf\xf6\xb7\xcf",                     // Warlock
+	4056 => "\xb7\xb9\xc0\xce\xc1\xae",                         4062 => "\xb7\xb9\xc0\xce\xc1\xae",                          4098 => "\xb7\xb9\xc0\xce\xc1\xae",             // Ranger
+	4057 => "\xbe\xc6\xc5\xa9\xba\xf1\xbc\xf3",                 4063 => "\xbe\xc6\xc5\xa9\xba\xf1\xbc\xf3",                  4099 => "\xbe\xc6\xc5\xa9\xba\xf1\xbc\xf3",     // Arch_Bishop
+	4058 => "\xb9\xcc\xc4\xc9\xb4\xd0",                         4064 => "\xb9\xcc\xc4\xc9\xb4\xd0",                          4100 => "\xb9\xcc\xc4\xc9\xb4\xd0",             // Mechanic
+	4059 => "\xb1\xe6\xb7\xce\xc6\xbe\xc5\xa9\xb7\xce\xbd\xba", 4065 => "\xb1\xe6\xb7\xce\xc6\xbe\xc5\xa9\xb7\xce\xbd\xba",  4101 => "\xb1\xe6\xb7\xce\xc6\xbe\xc5\xa9\xb7\xce\xbd\xbaº", // Guillotine_Cross
 
-	4066 => "°¡µå",             4073 => "°¡µå",             4102 => "°¡µå",            // Royal_Guard
-	4067 => "¼Ò¼ ·¯",       4074 => "¼Ò¼ ·¯",       4103 => "¼Ò¼ ·¯",      // Sorcerer
-	4068 => "Å¬¶ó¿î",           4075 => "Å¬¶ó¿î",           4104 => "Å¬¶ó¿î",          // Minstrel
-	4069 => "¿ø´õ·¯",           4076 => "¿ø´õ·¯",           4105 => "¿ø´õ·¯",          // Wanderer
-	4070 => "½´¶ó",           4077 => "½´¶ó",           4106 => "½´¶ó",          // Sura
-	4071 => "¦³×¸¯",            4078 => "¦³×¸¯",            4107 => "¦³×¸¯",           // Genetic
-	4072 => "½¦µµ¿ìÃ¼ÀÌ¼",  4079 => "½¦µµ¿ìÃ¼ÀÌ¼",  4108 => "½¦µµ¿ìÃ¼ÀÌ¼", // Shadow_Chaser
+	4066 => "\xb0\xa1\xb5\xe5",                                 4073 => "\xb0\xa1\xb5\xe5",                                  4102 => "\xb0\xa1\xb5\xe5",                 // Royal_Guard
+	4067 => "\xbc\xd2\xbc\xad\xb7\xaf",                         4074 => "\xbc\xd2\xbc\xad\xb7\xaf",                          4103 => "\xbc\xd2\xbc\xad\xb7\xaf",         // Sorcerer
+	4068 => "\xb9\xce\xbd\xba\xc6\xae\xb7\xb2",                 4075 => "\xb9\xce\xbd\xba\xc6\xae\xb7\xb2",                  4104 => "\xb9\xce\xbd\xba\xc6\xae\xb7\xb2", // Minstrel
+	4069 => "\xbf\xf8\xb4\xf5\xb7\xaf",                         4076 => "\xbf\xf8\xb4\xf5\xb7\xaf",                          4105 => "\xbf\xf8\xb4\xf5\xb7\xaf",         // Wanderer
+	4070 => "\xbd\xb4\xb6\xf3",                                 4077 => "\xbd\xb4\xb6\xf3",                                  4106 => "\xbd\xb4\xb6\xf3",                 // Sura
+	4071 => "\xc1\xa6\xb3\xd7\xb8\xaf",                         4078 => "\xc1\xa6\xb3\xd7\xb8\xaf",                          4107 => "\xc1\xa6\xb3\xd7\xb8\xaf",         // Genetic
+	4072 => "\xbd\xa6\xb5\xb5\xbf\xec\xc3\xbc\xc0\xcc\xbc\xad", 4079 => "\xbd\xa6\xb5\xb5\xbf\xec\xc3\xbc\xc0\xcc\xbc\xad",  4108 => "\xbd\xa6\xb5\xb5\xbf\xec\xc3\xbc\xc0\xcc\xbc\xad",// Shadow_Chaser
 
 	// Mount
-	4080 => "·é³ªÀÌÆ®»Ú¶ì",     4081 => "·é³ªÀÌÆ®»Ú¶ì",     4109 => "·é³ªÀÌÆ®»Ú¶ì", // Rune_Knight2
-	4082 => "±×¸®Æù°¡µå",       4083 => "±×¸®Æù°¡µå",       4110 => "±×¸®Æù°¡µå",   // Royal_Guard2
-	4084 => "·¹ÀÎÁ®´Á´ë",       4085 => "·¹ÀÎÁ®´Á´ë",       4111 => "·¹ÀÎÁ®´Á´ë",   // Ranger2
-	4086 => "¹ÌÄÉ´Ð¸äµÅÁö",     4087 => "¹ÌÄÉ´Ð¸äµÅÁö",     4112 => "¹ÌÄÉ´Ð¸äµÅÁö", // Mechanic2
+	4080 => "\xb7\xe9\xb3\xaa\xc0\xcc\xc6\xae\xbb\xda\xb6\xec", 4081 => "\xb7\xe9\xb3\xaa\xc0\xcc\xc6\xae\xbb\xda\xb6\xec",  4109 => "\xb7\xe9\xb3\xaa\xc0\xcc\xc6\xae\xbb\xda\xb6\xec", // Rune_Knight2
+	4082 => "\xb1\xd7\xb8\xae\xc6\xf9\xb0\xa1\xb5\xe5",         4083 => "\xb1\xd7\xb8\xae\xc6\xf9\xb0\xa1\xb5\xe5",          4110 => "\xb1\xd7\xb8\xae\xc6\xf9\xb0\xa1\xb5\xe5",         // Royal_Guard2
+	4084 => "\xb7\xb9\xc0\xce\xc1\xae\xb4\xc1\xb4\xeb",         4085 => "\xb7\xb9\xc0\xce\xc1\xae\xb4\xc1\xb4\xeb",          4111 => "\xb7\xb9\xc0\xce\xc1\xae\xb4\xc1\xb4\xeb",         // Ranger2
+	4086 => "\xb9\xcc\xc4\xc9\xb4\xd0\xb8\xe4\xb5\xc5\xc1\xf6", 4087 => "\xb9\xcc\xc4\xc9\xb4\xd0\xb8\xe4\xb5\xc5\xc1\xf6",  4112 => "\xb9\xcc\xc4\xc9\xb4\xd0\xb8\xe4\xb5\xc5\xc1\xf6", // Mechanic2
 
-	4190 => "½´ÆÛ³ëºñ½ºÆ÷¸µ", 4191 => "½´ÆÛ³ëºñ½ºÆ÷¸µ", // Super_Baby_E
+	4190 => "\xbd\xb4\xc6\xdb\xb3\xeb\xba\xf1\xbd\xba\xc6\xf7\xb8\xb5", 4191 => "\xbd\xb4\xc6\xdb\xb3\xeb\xba\xf1\xbd\xba\xc6\xf7\xb8\xb5", // Super_Baby_E
 
 	4211 => "kagerou", //Job_Kagerou	4211
 	4212 => "oboro"    //Job_Oboro	4212
